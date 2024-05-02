4fffdb917358c0d88c47f60a1336575e97dcff6c
Seishin <gabinhalloss@gmail.com>
2024-05-02 15:58:14 +0200

    NextAuth - Authentification (patch &apos;)

--- app/administration/page.js
+++ app/administration/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Administration() {
     return (
         <main>
-            <p>Ah le sang t'es bieeeeng là</p>
+            <p>Ah le sang t&apos;es bieeeeng là</p>
             <Link href={"/auth/signout"}>Si tu veux plus être bieeeeeng</Link>
         </main>
     )
